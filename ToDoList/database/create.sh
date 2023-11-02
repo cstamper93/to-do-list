b88355506dad2f5c4425220de0e42970db036327
Corey Stamper <cstamper93@gmail.com>
2023-11-02 17:01:24 -0400

    added db data to application.properties

--- ToDoList/database/create.sh
+++ ToDoList/database/create.sh
@@ -4,5 +4,4 @@ BASEDIR=$(dirname $0)
 DATABASE=to_do_list_db
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql"
