36ad4a4976913936ffd93ea8f35d5d195e4af213
Corey Stamper <cstamper93@gmail.com>
2023-11-06 14:41:35 -0500

    added list view and components

---
+++ to_do_list_vue/src/views/ListView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list-view">
+    <h1 class="to-do-title">To Do List</h1>
+    <div class="current-list">
+        <list-contents/>
+    </div>
+  </div>
+</template>
+
+<script>
+import BackendService from '@/services/BackendService.js'
+import ListContents from '@/components/ListContents.vue'
+
+export default {
+    components: { ListContents },
+    name: 'list-view',
+    data() {
+        return {
+            listItems: []
+        }
+    },
+    created() {
+        BackendService.fetchAllListItems().then((response) => {
+            this.listItems = response.data;
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
